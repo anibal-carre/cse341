0c0a915f18081e4cfcc690b6bcf1dd5a499d5300
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2025-11-15 19:42:56 -0300

    fix error

--- data/database.js
+++ data/database.js
@@ -4,6 +4,6 @@ const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URL;
 const client = new MongoClient(uri);
 
-const getContactsCollection = () => client.db(process.env.DB_NAME || "sample_analytics").collection("customers");
+const getContactsCollection = () => client.db(process.env.DB_NAME || "project").collection("users");
 
 module.exports = { client, getContactsCollection };
